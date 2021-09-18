f9642bcb1883f1e013fb87f9d45ab23eddff2f4f
santhosh-raajaa <santhoshraajaa611@gmail.com>
2021-09-18 05:40:16 +0530

    test

--- websites/__init__.py
+++ websites/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-db=SqlAlchemy()
+db=SQLAlchemy()
 db_name="database.db"
 
 
---
+++ websites/models.py
@@ -0,0 +1,2 @@
+from __init__ import db
+from flask_login import UserMixin
